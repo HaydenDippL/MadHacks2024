b1306ea1d04e307f1cb8e3aa5f1dd45541727f2e
Hayden Dippel <hdippel@wisc.edu>
2024-11-10 09:45:26 -0600

    Minor spacing changes and ingredients remove integrated

--- ChefItUp/src/pages/index.tsx
+++ ChefItUp/src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function IndexPage() {
         <h1 className={title()}>Welcome to ChefItUp</h1>
         <p className={subtitle()}>All we you need to do is let us know what ingredients you have, and we'll let tou know the best recipes you can make based on your macro perferences.</p>
       </div>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row w-4/5 gap-4">
         <Ingredients />
         <Macros />
         <RecipeFinder />
